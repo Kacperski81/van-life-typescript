666d70f31b27ed730998ee6bdbd01db352019c97
Marcin Kacperski <kacperski81@gmail.com>
2024-07-05 19:15:42 +0100

    LoggedIn fixed

--- src/Components/LoggedIn.tsx
+++ src/Components/LoggedIn.tsx
@@ -1,15 +1,18 @@
-import { useNavigate } from "react-router-dom"
 import { useUser } from "../UserContext"
-
+import { useNavigate } from "react-router-dom"
 
 export function LoggedIn() {
+    const navigate = useNavigate()
     const { setUser } = useUser()
+
     return (
         <button
             className="bg-red-500 w-80 py-1 rounded-lg text-white"
             onClick={() => {
                 setUser(() => {
                     return {
+                        userId: "",
+                        email: "",
                         userName: "",
                         isLoggedIn: false
                     }
