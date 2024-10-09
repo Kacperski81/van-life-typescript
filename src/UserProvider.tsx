47b33bd5701cc9c804cf9487bdf45d3258d4054a
Marcin Kacperski <kacperski81@gmail.com>
2024-10-09 19:58:45 +0100

    updated UserProvider

--- src/UserProvider.tsx
+++ src/UserProvider.tsx
@@ -1,64 +1,10 @@
-// UserProvider.tsx
 import { PropsWithChildren, useReducer } from "react";
 import { UserProvider as Provider } from "./UserContext";
 import { hostReducer } from "./reducer/reducer";
-// import { UserState } from "./types";
+import { UserState } from "./types";
 import { initialUserState } from "./utils";
-// const initialState: UserState = {
-//   incomeDays: 10,
-//   vanType: "allVans",
-//   user: {
-//       userName: "",
-//       email: "",
-//       userId: "",
-//       isLoggedIn: false,
-//   },
-//   transactions: [
-//       {
-//           total: 0,
-//           date: "",
-//           id: "",
-//           userId: "",
-//           simple: {
-//             name: "",
-//             price: 0,
-//             reviewId: "",
-//           },
-//           rugged: {
-//             name: "",
-//             price: 0,
-//             reviewId: "",
-//           },
-//           luxury: {
-//             name: "",
-//             price: 0,
-//             reviewId: "",
-//           },
-//       }
-//   ],
-//   userVans: [
-//       {
-//           id: "",
-//           name: "",
-//           price: 0,
-//           description:"",
-//           imageUrl: "",
-//           type: "",
-//           hostId: ""
-//       }
-//   ],
-//   reviews: [
-//       {
-//           rating: 0,
-//           name: "",
-//           date: "",
-//           text: "",
-//           id: "",
-//           vanId: "",
-//       }
-//   ],
-// }
-const initialState = initialUserState;
+
+const initialState: UserState =initialUserState;
 
 export const UserProvider = ({ children }: PropsWithChildren) => {
   const [state, dispatch] = useReducer(hostReducer, initialState);
